112592cb30693651506a337b40e2364522d1434c
JEONG-BD <dev.byeongdu@gmail.com>
2023-10-13 20:54:39 -0700

    2023-10-14 module

---
+++ Chapter01/chapter01_15.py
@@ -0,0 +1,24 @@
+
+def add(x, y):
+    return x + y 
+
+def subtract(x, y):
+    return x - y 
+
+def multiply(x, y):
+    return x * y 
+
+def divide(x, y):
+    return x / y 
+
+def power(x, y):
+    return x ** y 
+    
+
+if __name__ == "__main__":
+    print("module")
+    print(add(15, 5))
+    print(subtract(15, 5))
+    print(multiply(5, 5))
+    print(divide(10, 2))
+    print(power(5, 3))
---
+++ Chapter01/chapter01_16.py
@@ -0,0 +1,10 @@
+import chapter01_15 
+import sys 
+import time 
+
+print(sys)
+print(sys.path)
+print(type(sys.path))
+
+if __name__ == "__main__":
+    print(chapter01_15.add(1, 3))
