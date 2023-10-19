137d78baaca79d72551bf26ea6c79eb556809566
JEONG-BD <dev.byeongdu@gmail.com>
2023-10-20 08:29:42 +0900

    2023-10-20 dictionary setdefault

---
+++ Chapter02/chapter02_10.py
@@ -0,0 +1,35 @@
+
+if __name__ == '__main__': 
+    print(__builtins__.__dict__)
+
+
+    t1 = (10, 20, (30, 40, 50))
+    t2 = (10, 20, [30, 40, 50])
+    # print(hash(t1))
+    # print(hash(t2))
+
+    print()
+    print()
+
+    source = (('k1', 'val1'),
+              ('k1', 'val2'), 
+              ('k2', 'val3'),
+              ('k2', 'val4'),
+              ('k2', 'val5'))
+    
+    new_dict1 = {}
+    new_dict2 = {} 
+
+    for k, v in source:
+        if k in new_dict1 : 
+            new_dict1[k].append(v)
+        else :
+            new_dict1[k]=[v] 
+        print(k, v)
+    print(new_dict1)
+
+
+    for k, v in source: 
+        new_dict2.setdefault(k, []).append(v)
+    
+    print(new_dict2)
