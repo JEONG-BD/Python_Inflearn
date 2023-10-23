2d30202b1339e0da21f55bbc0c65d4210a680804
JEONG-BD <dev.byeongdu@gmail.com>
2023-10-24 08:30:20 +0900

    2023-10-24 asyncio

---
+++ Chapter02/chapter02_23.py
@@ -0,0 +1,41 @@
+import io
+import os 
+import sys 
+import timeit
+import asyncio
+import threading 
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+ 
+
+start = timeit.default_timer()
+
+urls = ['http://daum.net', 
+       'https://naver.com', 
+       'http://mlbpark.donga.com', 
+       'https://tistory.com', 
+       'https://wemakeprice.com']
+
+async def fetch(url, executor):
+    print(f'Threading Name {threading.current_thread().getName} Start : {url}')
+    res = await loop.run_in_executor(executor, urlopen, url)
+    soup = BeautifulSoup(res.read(), 'html.parser')
+    # print(soup.prettify())
+    result_data = soup.title
+
+    return result_data 
+
+async def main():
+
+    executor = ThreadPoolExecutor(max_workers=10)
+    futures = [asyncio.ensure_future(fetch(url, executor)) for url in urls]
+
+    rst = await asyncio.gather(*futures)
+    print(f'Result : {rst}')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    duration = timeit.default_timer() - start
+    print(f'Time {duration}')
